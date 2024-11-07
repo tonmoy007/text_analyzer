1937a6e0a762c93345db647284e6870ca612c3c6
Saddam Hossain <tonmoy007@users.noreply.github.com>
2024-11-08 04:05:06 +0600

    [UPDATE] E2E Tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { AppModule } from '../src/app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 describe('AppController (e2e)', () => {
-  let app: INestApplication;
+  let app: NestFastifyApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleFixture.createNestApplication<NestFastifyApplication>(
+      new FastifyAdapter(),
+    );
     await app.init();
+
+    await app.getHttpAdapter().getInstance().ready();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  it('/ (GET)', async () => {
+    const result = await app.inject({
+      method: 'GET',
+      url: '/',
+    });
+    expect(result.statusCode).toEqual(200);
+    expect(result.body).toEqual('Hello World!');
+  });
+  it('/ (POST) LOGIN', () => {
+    app
+      .inject({
+        method: 'POST',
+        url: '/auth/login',
+        headers: { 'Content-Type': 'application/json' },
+        body: { email: 'admin@test.io', password: 'password' },
+      })
+      .then((result) => {
+        expect(result.statusCode).toEqual(201);
+      });
   });
 });
